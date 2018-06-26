cfc5453a7c71407f97012f6aed1c02627db0a636
manojkumarsmks <manojkumarsmks@gmail.com>
2018-06-26 00:08:53 -0600

    Check duplicates using sets

--- ContainsDuplicates.java
+++ ContainsDuplicates.java
@@ -1,10 +1,13 @@
 // https://leetcode.com/problems/contains-duplicate/description/
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContainsDuplicates {
 
 	public static void main(String[] args) {
-		int[] input = {3,1};
-		System.out.println(checkForDuplicatesBruteForce(input));
+		int[] input = {1,2,3,1};
+		System.out.println(checkUsingMaps(input));
 	}
 	
 	// Brute force method 
@@ -14,6 +17,32 @@ public class ContainsDuplicates {
 			for (int j = i+1; j < inputArray.length; j++) {
 				if(inputArray[i] == inputArray[j])
 					return true;
+			}
+		}
+		return false;
+	}
+	
+	// Sort and check if two consecutive indexes are same 
+	public static boolean checkBySorting(int[] inputArray) {
+		Arrays.sort(inputArray);
+		
+		for (int i = 0; i < inputArray.length-1; i++) {
+			if(inputArray[i] == inputArray[i+1])
+				return true;
+		}
+		return false;
+		
+	}
+	
+	// Using set to check if the element exists
+	public static boolean checkUsingMaps(int[] inputArray) {
+		
+		Set<Integer> set = new HashSet<Integer>();
+		for (int i = 0; i < inputArray.length; i++) {
+			if(set.contains(inputArray[i]))
+				return true;
+			else {
+				set.add(inputArray[i]);
 			}
 		}
 		return false;
