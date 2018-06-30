43fe3074adcc82e6ab216738357f988533b0f4b2
manojkumarsmks <manojkumarsmks@gmail.com>
2018-06-30 13:07:38 -0600

    Best solution

--- FindAllDuplicates.java
+++ FindAllDuplicates.java
@@ -7,7 +7,9 @@ public class FindAllDuplicates {
 
 	public static void main(String[] args) {
 		int[] array = {4,3,2,7,8,2,3,1};
-		List<Integer> result = findDisappearedNumbers(array);
+		List<Integer> result = disappearedNumbers(array);
+		
+		//System.out.println(result.size());
 		for (Integer integer : result) {
 			System.out.println(integer);
 		}
@@ -32,8 +34,26 @@ public class FindAllDuplicates {
 				result.add(i+1);
 		}
 		
-		return result;
-		
+		return result;	
 	}
+	
+	// Time Complexity - O(n)
+	// Space Complexity - O(1)
+	public static List<Integer> disappearedNumbers(int[] nums) {
+		List<Integer> result = new ArrayList<Integer>();
+		
+		for (int i = 0; i < nums.length; i++) {
+			if(nums[Math.abs(nums[i])-1] > 0) 
+				nums[Math.abs(nums[i])-1] = -nums[Math.abs(nums[i])-1];	
+		}
+		
+		for (int i = 0; i < nums.length; i++) {
+			if(nums[i] >0) 
+				result.add(i+1);
+		}
+		return result;
+	}
+	
+	
 
 }
